3aef9018fd56d4f6d3f379f3214423191dab2a35
Abhinash216 <abinash10abi@gmail.com>
2025-10-24 08:25:15 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App{
+  public static void ShowAppVersion(){
+  System.out.println("App Version:2.5.1");{
+}
+}
+  public static void main(String [] args);{
+  ShowAppVersion();
+}
+
+
+}
